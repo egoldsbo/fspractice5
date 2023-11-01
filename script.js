52ea7dea354bfa7b971966b5e0d687c26e6fb4bb
Evan <egoldsbo@gmail.com>
2023-10-31 22:16:05 -0700

    changed videos added 2 video elements

--- script.js
+++ script.js
@@ -1,137 +1,140 @@
-
-
- var currentdirectory="mostcommonsigns";
- var currentwordset=[];
- var wordsetindex=0;
+var currentdirectory = "mostcommonsigns";
+var currentwordset = [];
+var wordsetindex = 0;
 
 let originalPlaybackRate = 1;
 var speeds = [.12, .25, .5, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3];
-var speedIndex= 3;
-var videoName="something";
-const videoElement = document.getElementById("videoPlayer");
-            const buttonElement = document.getElementById("revealword");
+var speedIndex = 3;
+var videoName = "something";
+
+// Managing two video elements
+const videoElements = [document.getElementById("videoPlayer1"), document.getElementById("videoPlayer2")];
+let activeVideoIndex = 0; // Index to track which video is currently playing
+
+// Initialize the first video element
+videoElements[0].src = generateVideoPath();
+videoElements[0].playbackRate = speeds[speedIndex];
+videoElements[0].play();
+
+// Button to reveal the video name
+const buttonElement = document.getElementById("revealword");
 
 buttonElement.onmousedown = () => {
-                buttonElement.textContent = videoName;
-            };
+    buttonElement.textContent = videoName;
+};
 
-            buttonElement.ontouchstart = () => {
-                buttonElement.textContent = videoName;
-            };
+buttonElement.ontouchstart = () => {
+    buttonElement.textContent = videoName;
+};
 
-            buttonElement.onmouseup = () => {
-                buttonElement.textContent = "REVEAL FILE NAME";
-            };
+buttonElement.onmouseup = () => {
+    buttonElement.textContent = "REVEAL FILE NAME";
+};
 
-        function fetchRandomVideo() {
+function generateVideoPath() {
+    if (currentwordset.length === 0) {
+        currentwordset = mostcommonsigns;
+    }
 
-            if(currentwordset.length==0){
+    wordsetindex++;
+    if (wordsetindex >= currentwordset.length) {
+        wordsetindex = 0;
+        shuffleArray(currentwordset);
+    }
 
-                currentwordset=mostcommonsigns;
-
-            }
-            
-                    wordsetindex++;
-                    if(wordsetindex>=currentwordset.length){
-                        wordsetindex=0;
-                        shuffleArray(currentwordset)
-
-                    }
-
-                    const videoElement = document.getElementById("videoPlayer");
-                    var playdirectory="vids/"+currentdirectory+"/"+currentwordset[wordsetindex];
-                    console.log(playdirectory);
-                    videoElement.style.display = "block";
-                    videoElement.src = playdirectory;
-                    const videoPath = videoElement.src;
-                    videoName = videoPath.split("/").pop();
-                    videoElement.playbackRate = speeds[speedIndex];
-                    videoElement.play();
-                }
-        
-
-
-        function changeWordset(){
-            wordsetindex=0;
-            const dir = document.getElementById("dirSelect").value;
-            currentdirectory=dir;
-                if(dir=="mostcommonsigns"){
-                        currentwordset=mostcommonsigns;
-                }
-                if(dir=="mostcommonsigns2"){
-                        currentwordset=mostcommonsigns2;
-                }
-                if(dir=="dailymoth"){
-                        currentwordset=dailymoth;
-                }
-                if(dir=="inthewild"){
-                        currentwordset=inthewild;
-                }
-                shuffleArray(currentwordset);
-        }
-
-    function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    // Generate a random index from 0 to i
-    const j = Math.floor(Math.random() * (i + 1));
-    
-    // Swap elements array[i] and array[j]
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    var playdirectory = "vids/" + currentdirectory + "/" + currentwordset[wordsetindex];
+    console.log(playdirectory);
+    videoName = playdirectory.split("/").pop();
+    return playdirectory;
 }
 
+function switchVideos() {
+    // Calculate next video index
+    let nextVideoIndex = (activeVideoIndex + 1) % videoElements.length;
 
-        function playAgain() {
-            const video = document.getElementById("videoPlayer");
-            video.style.display = "block";
-            video.playbackRate = speeds[speedIndex];
-            video.currentTime = 0;
-            video.play();
-        }
+    // Update video source and prepare next video
+    videoElements[nextVideoIndex].src = generateVideoPath();
+    videoElements[nextVideoIndex].playbackRate = speeds[speedIndex];
+    videoElements[nextVideoIndex].style.display = "block";
 
-        function playSlower() {
-            const video = document.getElementById("videoPlayer");
-            video.style.display = "block";
-            video.playbackRate = 0.5;
-            video.currentTime = 0;
-            video.play();
-        }
+    // Hide the current video and show the next one
+    videoElements[activeVideoIndex].style.display = "none";
+    videoElements[nextVideoIndex].play();
+    
+    // Swap active video index
+    activeVideoIndex = nextVideoIndex;
+}
 
-        function openInNewTab(urlBase) {
-            const videoElement = document.getElementById("videoPlayer");
-            const videoPath = videoElement.src;
-            const videoName = videoPath.split("/").pop();
-            const choppedName = videoName.slice(0, -4); // Remove last 4 chars (.mp4)
-            window.open(urlBase + choppedName, '_blank');
-        }
+function changeWordset() {
+    wordsetindex = 0;
+    const dir = document.getElementById("dirSelect").value;
+    currentdirectory = dir;
 
-        function changeSpeed(isIncrease) {
-            const video = document.getElementById("videoPlayer");
-            if(isIncrease&&speedIndex<speeds.length-1){
-                speedIndex++;
-            }
+    if (dir === "mostcommonsigns") {
+        currentwordset = mostcommonsigns;
+    } else if (dir === "mostcommonsigns2") {
+        currentwordset = mostcommonsigns2;
+    } else if (dir === "dailymoth") {
+        currentwordset = dailymoth;
+    } else if (dir === "inthewild") {
+        currentwordset = inthewild;
+    }
 
-            if (!isIncrease&&speedIndex>0){
-            speedIndex--;
-        }
+    shuffleArray(currentwordset);
+    switchVideos(); // Automatically load and play the first video from the new wordset
+}
 
-            video.playbackRate = speeds[speedIndex];
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
-            document.getElementById("currentSpeed").textContent = video.playbackRate.toFixed(2) + 'x';
-        }
+function playAgain() {
+    const video = videoElements[activeVideoIndex];
+    video.style.display = "block";
+    video.playbackRate = speeds[speedIndex];
+    video.currentTime = 0;
+    video.play();
+}
 
-        
+function playSlower() {
+    const video = videoElements[activeVideoIndex];
+    video.style.display = "block";
+    video.playbackRate = 0.5;
+    video.currentTime = 0;
+    video.play();
+}
 
-        function aboutSite() {
-            const video = document.getElementById("videoPlayer");
-            const aboutInfo = document.getElementById("aboutInfo");
-            if( video.style.display == "block"){
-                video.style.display = "none";
-                aboutInfo.style.display = "block";
-        }
+function openInNewTab(urlBase) {
+    const videoPath = videoElements[activeVideoIndex].src;
+    const videoName = videoPath.split("/").pop();
+    const choppedName = videoName.slice(0, -4); // Remove last 4 chars (.mp4)
+    window.open(urlBase + choppedName, '_blank');
+    
+}
 
-        else{video.style.display = "block";
-      }
+function changeSpeed(isIncrease) {
+    const video = videoElements[activeVideoIndex];
+    if (isIncrease && speedIndex < speeds.length - 1) {
+        speedIndex++;
+    } else if (!isIncrease && speedIndex > 0) {
+        speedIndex--;
+    }
 
-        }
+    video.playbackRate = speeds[speedIndex];
+    document.getElementById("currentSpeed").textContent = video.playbackRate.toFixed(2) + 'x';
+}
+
+function aboutSite() {
+    const aboutInfo = document.getElementById("aboutInfo");
+    if (videoElements[activeVideoIndex].style.display === "block") {
+        videoElements[activeVideoIndex].style.display = "none";
+        aboutInfo.style.display = "block";
+    } else {
+        videoElements[activeVideoIndex].style.display = "block";
+        aboutInfo.style.display = "none";
+    }
+}
