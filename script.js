cddc76402e74ea4cb534b25ba43c24271da76f79
Evan <egoldsbo@gmail.com>
2023-11-02 20:32:32 -0700

    Fix video player controls and remove unused code

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ var videoName = "something";
 
 // Managing two video elements
 const videoElements = [document.getElementById("videoPlayer1"), document.getElementById("videoPlayer2")];
-let activeVideoIndex = 0;
-// Index to track which video is currently playing
+let activeVideoIndex = 0; // Index to track which video is currently playing
 
 // Initialize the first video element
 videoElements[0].src = generateVideoPath();
@@ -20,20 +19,17 @@ videoElements[0].play();
 // Button to reveal the video name
 const buttonElement = document.getElementById("revealword");
 
-buttonElement.onmousedown = ()=>{
+buttonElement.onmousedown = () => {
     buttonElement.textContent = videoName;
-}
-;
+};
 
-buttonElement.ontouchstart = ()=>{
+buttonElement.ontouchstart = () => {
     buttonElement.textContent = videoName;
-}
-;
+};
 
-buttonElement.onmouseup = ()=>{
+buttonElement.onmouseup = () => {
     buttonElement.textContent = "REVEAL FILE NAME";
-}
-;
+};
 
 function generateVideoPath() {
     if (currentwordset.length === 0) {
@@ -64,7 +60,7 @@ function switchVideos() {
     // Hide the current video and show the next one
     videoElements[activeVideoIndex].style.display = "none";
     videoElements[nextVideoIndex].play();
-
+    
     // Swap active video index
     activeVideoIndex = nextVideoIndex;
 }
@@ -84,19 +80,18 @@ function changeWordset() {
         currentwordset = inthewild;
     }
 
-    else if (dir === "commonwordendings") {
-        currentwordset = commonwordendings;
-    }
+else if (dir === "commonwordendings") {
+    currentwordset = commonwordendings;
+}
 
     shuffleArray(currentwordset);
-    switchVideos();
-    // Automatically load and play the first video from the new wordset
+    switchVideos(); // Automatically load and play the first video from the new wordset
 }
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i],array[j]] = [array[j], array[i]];
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
@@ -120,10 +115,9 @@ function playSlower() {
 function openInNewTab(urlBase) {
     const videoPath = videoElements[activeVideoIndex].src;
     const videoName = videoPath.split("/").pop();
-    const choppedName = videoName.slice(0, -4);
-    // Remove last 4 chars (.mp4)
+    const choppedName = videoName.slice(0, -4); // Remove last 4 chars (.mp4)
     window.open(urlBase + choppedName, '_blank');
-
+    
 }
 
 function changeSpeed(isIncrease) {
